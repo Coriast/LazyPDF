1263669c0fdcb7fed5c470a4fbd80bfe07e98147
helpmeagain <felipe.comarques1208@gmail.com>
2025-10-13 21:59:05 -0300

    build: added initial dependencies

---
+++ app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen text-center">
+    <h1 class="text-5xl font-bold text-gray-800 mb-3">Hello World!</h1>
+    <p class="text-lg text-gray-600 tracking-wide">Lazy PDF</p>
+  </div>
+</template>
