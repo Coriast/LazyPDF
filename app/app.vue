b30559fc1351dd6ce843b9b9e6940987d3fe39c1
Pedro Willian de Oliveira Vieira <pedrowillianpesquisador@gmail.com>
2025-10-15 16:23:01 -0300

    feat: drag and drop of pdf file

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="h-dvh overflow-y-auto">
-    <p class="text-center mt-5 text-2xl">Teste</p>
-    <NuxtPage />
-  </div>
+  <UApp>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,60 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen text-center">
-    <h1 class="text-5xl font-bold text-gray-800 mb-3">Hello World!</h1>
-    <p class="text-lg text-gray-600 tracking-wide">Lazy PDF</p>
-    <a href="/test">Ir para a página "test"</a>
-  </div>
+  <UDashboardPanel id="home">
+      <template #header>
+        <div class="flex">
+          <a href="/test">Teste</a>
+          <UFileUpload v-model="filePDF" @change="handleUploadPDF" accept="pdf" variant="button" class="w-20 h-10"/>
+        </div>
+      </template>
+      <template #body>
+        <canvas id="render"></canvas>
+      </template>
+  </UDashboardPanel>
 </template>
+
+<script setup>
+import { getDocument, GlobalWorkerOptions } from "pdfjs-dist";
+import worker from "pdfjs-dist/build/pdf.worker.mjs?url";
+
+const filePDF = ref(null);
+
+GlobalWorkerOptions.workerSrc = worker;
+
+const handleUploadPDF = (event) => {
+  let fileReader = new FileReader();
+
+  fileReader.onload = (event) => {
+    let resultArray = new Uint8Array(event.target.result);
+
+    let loadingTask = getDocument(resultArray);
+    loadingTask.promise.then(function(pdf) {
+      pdf.getPage(1).then(function(page) {
+        let scale = 1.5;
+        let viewport = page.getViewport({scale: scale});
+
+        let canvas = document.getElementById("render");
+        let context = canvas.getContext("2d");
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        let renderContext = {
+          canvasContext: context,
+          viewport: viewport
+        };
+
+        let renderTask = page.render(renderContext);
+        renderTask.promise.then(function () {
+
+        });
+      }, function (reason) {
+        console.error(reason);
+      })
+    });
+  }
+
+  fileReader.readAsArrayBuffer(filePDF.value);
+}
+
+
+
+</script>
