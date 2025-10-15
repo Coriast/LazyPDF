682cae69e75e1b4ce70a8031da905b849c309e90
Pedro Willian de Oliveira Vieira <pedrowillianpesquisador@gmail.com>
2025-10-15 20:42:21 -0300

    feat: PDFViewer component working

--- app/pages/pdfViewer.vue
+++ app/pages/pdfViewer.vue
@@ -1,40 +1,65 @@
-<template>      
-  <canvas id="render"></canvas>
+<template>  
+  <div id="viewerContainer" class="overflow-auto absolute w-full h-full">
+    <div id="viewer" class="pdfViewer"></div>
+  </div>
 </template>
 
 <script setup>
 import { getDocument, GlobalWorkerOptions } from "pdfjs-dist";
 import worker from "pdfjs-dist/build/pdf.worker.mjs?url";
 
+import { EventBus, PDFFindController, PDFLinkService, PDFViewer } from "pdfjs-dist/web/pdf_viewer.mjs";
+
 GlobalWorkerOptions.workerSrc = worker;
 
 const hardCodedFilePath = "sample.pdf"
 
-onMounted(() => {
-  let loadingTask = getDocument(hardCodedFilePath);
-  loadingTask.promise.then(function(pdf) {
-    pdf.getPage(1).then(function(page) {
-      let scale = 1.5;
-      let viewport = page.getViewport({scale: scale});
+const CMAP_URL = "../../node_modules/pdfjs-dist/cmaps/";
+const CMAP_PACKED = true;
+const SEARCH_FOR = "";
 
-      let canvas = document.getElementById("render");
-      let context = canvas.getContext("2d");
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
+onMounted(async () => {
+  const container = document.getElementById('viewerContainer');
 
-      let renderContext = {
-        canvasContext: context,
-        viewport: viewport
-      };
-
-      let renderTask = page.render(renderContext);
-      renderTask.promise.then(function () {
-
-      });
-    }, function (reason) {
-      console.error(reason);
-    })
+  const eventBus = new EventBus();
+  const pdfLinkService = new PDFLinkService({
+    eventBus
   });
+
+  const pdfFindController = new PDFFindController({
+    eventBus,
+    linkService: pdfLinkService
+  });
+
+  const pdfViewer = new PDFViewer({
+    container: container,
+    eventBus: eventBus,
+    linkService: pdfLinkService,
+    findController: pdfFindController
+  });
+  pdfLinkService.setViewer(pdfViewer);
+
+  eventBus.on("pagesinit", () => {
+    pdfViewer.currentScaleValue = "page-width";
+
+    if(SEARCH_FOR)
+    {
+      eventBus.dispatch("find", {type: "", query: SEARCH_FOR });
+    }
+  });
+
+  const loadingTask = getDocument({
+    url: hardCodedFilePath,
+    cMapUrl: CMAP_URL,
+    cMapPacked: CMAP_PACKED,
+    enableXfa: true
+  });
+
+  const pdfDocument = await loadingTask.promise;
+
+  pdfViewer.setDocument(pdfDocument);
+
+  pdfLinkService.setDocument(pdfDocument, null);
 });
 
 </script>
